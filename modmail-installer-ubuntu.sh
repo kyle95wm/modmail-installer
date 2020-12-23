d233b6caea7623d9afb4afc46bb87e058eeef1cf
kyle95wm <kyle95wm@gmail.com>
2020-12-22 22:36:13 -0500

    Give the user an option for what kind of log viewer to use

--- modmail-installer-ubuntu.sh
+++ modmail-installer-ubuntu.sh
@@ -19,12 +19,16 @@ fi
 pipenv install
 echo "Installed Deps!"
 cd ..
-git clone https://github.com/kyb3r/logviewer.git
-cd logviewer
-echo "Cloned LogViewer, Installing deps"
-pipenv install
-echo "Installed Deps Of LogViewer"
-cd ..
+echo "We are about to set up your log viewer. Do you want to set up your log viewer on THIS machine? [y/n]. If you choose not to, you will be prompted to enter a Heroku app name. If you choose to have a local log viewer instead, you must enter your machine's IP address, followed by port 8000 (http://10.10.10.10:8000)"
+read locallogs
+if [ -z "$locallogs" ] || [ "$locallogs" == "y" ] || [ "$locallogs" == "Y" ] ; then
+    git clone https://github.com/kyb3r/logviewer.git
+    cd logviewer
+    echo "Cloned LogViewer, Installing deps"
+    pipenv install
+    echo "Installed Deps Of LogViewer"
+    cd ..
+fi
 cd modmail
 touch .env
 echo "Enter TOKEN [Your discord bot's token.]"
@@ -44,13 +48,22 @@ echo OWNERS=$own >>.env
 echo "Enter CONNECTION_URI [Mongo DB connection URI for self-hosting your data.]"
 read uri
 echo CONNECTION_URI=$uri >>.env
-echo "Enter LOG_URL [This can be the ip and port of your vps which you will set later]"
-read lu
-echo LOG_URL=$lu >>.env
-echo "Completed Setup for modmail, not settings for logviewer"
-cd ..
-cd logviewer
-touch .env
-echo CONNECTION_URI=$uri >>.env
-echo "Completed Setup!"
+if [ $locallogs == "y" ] || [ -z $locallogs ] ; then
+    echo "Enter LOG_URL [This is the ip and port of your vps which was mentioned earlier.]"
+    read lu
+    echo LOG_URL=$lu >>.env
+fi
+if [ $locallogs == "n" ] || [ $locallogs == "N" ] ; then
+    echo "Enter LOG_URL [This is the APP NAME of your Heroku log viewe application. You do not need to include the full URL, just your app's name.]"
+    read lu
+    echo LOG_URL=https://$lu.herokuapp.com >>.env
+fi
+if [ -z "$locallogs" ] || [ "$locallogs" == "y" ] || [ "$locallogs" == "Y" ] ; then
+    echo "Completed Setup for modmail, now settings for logviewer"
+    cd ..
+    cd logviewer
+    touch .env
+    echo CONNECTION_URI=$uri >>.env
+fi
+echo "Completed Setup! Before you try to run your apps, run: 'source .profile' in your home folder."
 exit 0
